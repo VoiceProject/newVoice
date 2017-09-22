016de2f1872a942bb72d785f71a954c715f28912
BiYangJun <html2017byj@163.com>
2017-09-22 09:04:25 +0800

    9-22 9:04

--- project/src/scripts/components/MineComponent/loginComponent.js
+++ project/src/scripts/components/MineComponent/loginComponent.js
@@ -5,26 +5,32 @@ class LunchComponent extends React.Component {
         super(props,context)
 
         this.state={
-          phonenum:[]
+          phonenum:[],
+          checkInp:true
         }
     }
    phone(e){
         let reg=/^1\d{10}$/;
         reg.test(e.target.value)?
-        this.setState={
+        this.setState({
             phonenum:e.target.value
-        }:"";
+        }):"";
    }
     psd(e){
         let reg=/^1\d{10}$/;
         reg.test(e.target.value)?
-        this.setState={
+        this.setState({
             phonenum:e.target.value
-        }:"";
+        }):"";
+   }
+   changeCheckbox(){
+       this.setState({
+           checkInp:!this.state.checkInp
+       })
    }
     render(){
         // console.log(this)
-        let {value}=this.state;
+        // let {value}=this.state;
         return (           
           <div className="login_full">
              <p>我和你之间就差这一步了，别走</p>
@@ -37,7 +43,7 @@ class LunchComponent extends React.Component {
                 <input type="text" onBlur={this.psd} required placeholder="请输入用户密码"/>
              </div>
              <div className="login_checkbox">
-                 <input type="checkbox" checked/>
+                 <input type="checkbox" onClick={this.changeCheckbox.bind(this)} checked={this.state.checkInp?"checked":""}/>
                  <span>阅读并接受<a href="http://www.gd12355.org/wechat/view/logoin/user_Agreement.html" target="_blank">《青年之声用户服务协议》</a></span>
              </div>
              <div className="login_box">
