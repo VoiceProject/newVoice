b750942f1aeaa3084e79653713db2cc31643b4c3
luoyangwangyiyx@163.com <luoyangwangyiyx@163.com>
2017-09-24 14:53:23 +0800

    14:53

--- project/src/scripts/components/HelpComponent/Searchinput.js
+++ project/src/scripts/components/HelpComponent/Searchinput.js
@@ -1,11 +1,13 @@
-
+import Fetch from '../../modules/fetch'
 import {Link} from 'react-router';
 class Searchinput extends React.Component {   
     constructor(props,context){
         super(props,context)
 
         this.state={
-            inputwidth:'100%'
+            inputwidth:'100%',
+            value:'',
+            json:{}
         }
     }
    search(){
@@ -13,14 +15,32 @@ class Searchinput extends React.Component {
             inputwidth:'77%'
         })
    }
+    inputvalue(e){
+        this.setState({
+            value:e.target.value
+        })
+    }
+    searchkeyword(){
+        let that=this;
+        let {value,json}=this.state;       
+        let time=new Date().getTime();
+        Fetch.Get('http://api.12355.net/activity/offlineActivity/list?sitenavDid=440000&keywords='+value+'&pageIndex=1&pageSize=6&_='+time+'',{}).then((res)=>{
+            return res.json()
+        }).then((json)=>{
+            console.log(json);
+            that.setState({
+                json:json
+            })
+        })
+    }
     render(){
         let {inputwidth}=this.state;
         return (
           <div className="search_box">
-                <input type="text" placeholder={this.props.alt} onClick={this.search.bind(this)} className={inputwidth=='100%'?'':'search_box__short'} />
+                <input type="text" placeholder={this.props.alt} onChange={this.inputvalue.bind(this)} onClick={this.search.bind(this)} className={inputwidth=='100%'?'':'search_box__short'} />
                 {
                     inputwidth=='77%'?
-                    <a>搜索</a>:''
+                    <a onClick={this.searchkeyword.bind(this)}>搜索</a>:''
                 }
           </div>
         )
